56acbfee936af5410e91fd58f5c95caa24b9eeac
Océane Patiny <oceane@patiny.com>
2020-04-21 17:06:05 +0200

    chore: modify App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+import Input from './components/Input';
+import ShowMF from './components/ShowMF';
+
 import './App.css';
 
 function App() {
+  const [mf, setMF] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Input setMF={setMF} />
+      <ShowMF mf={mf} />
     </div>
   );
 }
