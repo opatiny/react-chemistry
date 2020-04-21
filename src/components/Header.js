d55feac8293f428459cdf30216855561ae7bda39
Océane Patiny <oceane@patiny.com>
2020-04-21 17:05:03 +0200

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+// function-based component! (vs class-based components)
+
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header style={headerStyle}>
+      <div>
+        <h1>Molecular structures calculator</h1>
+      </div>
+    </header>
+  );
+}
+
+const headerStyle = {
+  background: '#334',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px',
+};
