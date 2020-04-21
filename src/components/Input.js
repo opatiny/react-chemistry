39c72879a80606f554dd9137ac86206953b1c8d3
Océane Patiny <oceane@patiny.com>
2020-04-21 17:05:20 +0200

    feat: add Input component

---
+++ src/components/Input.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+export default function Input(props) {
+  // this is a component state
+  const [state, setState] = useState({
+    input: '',
+  });
+
+  function onSubmit(event) {
+    event.preventDefault(); // ?
+    props.setMF(state.input);
+    setState({ input: '' });
+  }
+
+  function handleNewEntry(event) {
+    setState({ [event.target.name]: event.target.value });
+  }
+
+  return (
+    <form onSubmit={onSubmit} style={{ display: 'flex' }}>
+      <input
+        type="text"
+        name="input"
+        style={{ flex: '10', padding: '5px' }}
+        placeholder="Enter a simple molecular formula ..."
+        value={state.input}
+        onChange={handleNewEntry}
+      />
+      <input
+        type="submit"
+        value="Submit"
+        className="button"
+        style={{ flex: '1' }}
+      />
+    </form>
+  );
+}
