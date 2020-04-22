aff48077e09109835f87a6af2b62a5f591bf5539
Océane Patiny <oceane@patiny.com>
2020-04-22 11:24:03 +0200

    chore (App): implement Molecules component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import Input from './components/Input';
+import Molecules from './components/Molecules';
 import ShowMF from './components/ShowMF';
 
 import './App.css';
+import getIdCodes from './getIdCodes';
 
 function App() {
   const [mf, setMF] = useState('');
+  const [idCodes, setIdCodes] = useState([]);
+
+  useEffect(() => {
+    if (mf) getIdCodes(mf).then((results) => setIdCodes(results));
+  }, [mf]); // the callback is executed only if mf changes
 
   return (
     <div className="App">
       <Header />
       <Input setMF={setMF} />
       <ShowMF mf={mf} />
+      <Molecules idCodes={idCodes} />
     </div>
   );
 }
