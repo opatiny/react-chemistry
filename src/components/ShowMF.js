ca651caab212589711873f3c55d725760d47edff
Océane Patiny <oceane@patiny.com>
2020-04-22 11:23:27 +0200

    chore: change mf style

--- src/components/ShowMF.js
+++ src/components/ShowMF.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { MF } from 'react-mf';
 
-export default function Input(props) {
+export default function ShowMF(props) {
   return (
-    <div style={outputStyle}>
-      <MF mf={props.mf} />
+    <div style={mfStyle}>
+      <style>
+        {`.mfTxtStyle {
+                font-weight: bold;
+                font-size: 20;
+              }`}
+      </style>
+
+      <MF className="mfTxtStyle" mf={props.mf} />
     </div>
   );
 }
 
-const outputStyle = {
+const mfStyle = {
   background: '#f4f4f4',
   padding: '20px',
   borderBottom: '1px solid #ccc ',
+  textAlign: 'center',
 };
