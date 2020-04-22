e2006238459561e0ae6c4fbd4cb19a81e39d3523
Océane Patiny <oceane@patiny.com>
2020-04-22 14:24:38 +0200

    Updates

---
+++ precache-manifest.cbe41a48ba6df4415fd33879f7dd77ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd71ef77aee84321d65debc05837380",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26218f57f184fc1df93b",
+    "url": "./static/css/main.92ac6bb3.chunk.css"
+  },
+  {
+    "revision": "2eea27a38e861e00ac43",
+    "url": "./static/js/2.3838f081.chunk.js"
+  },
+  {
+    "revision": "5cbd24537554ba26f4f69f856dee861a",
+    "url": "./static/js/2.3838f081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26218f57f184fc1df93b",
+    "url": "./static/js/main.c4d3b314.chunk.js"
+  },
+  {
+    "revision": "6bdc15947e6b8f0a3204",
+    "url": "./static/js/runtime-main.e503e403.js"
+  }
+]);
