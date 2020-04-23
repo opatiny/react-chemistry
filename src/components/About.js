4ab9d267459b7089b34ee0de57a614193ba272db
Océane Patiny <oceane@patiny.com>
2020-04-23 08:58:34 +0200

    chore: enhance About

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import './About.css';
 
 // the <React.Fragment> tag is a tag that is empty -> it corresponds to nothing in the DOM
 export default function About() {
   return (
-    <React.Fragment>
-      <div style={aboutStyle}>
-        <h1>About</h1>
-        <p>Molecular structures calculator app</p>
-        <p>Version: v1.0.0</p>
-        <p>
-          Github repository:{' '}
-          <a
-            className="link"
-            href="https://github.com/opatiny/react-mol-structures"
-          >
-            https://github.com/opatiny/react-mol-structures
-          </a>
-        </p>
-      </div>
-    </React.Fragment>
+    <div className="aboutStyle">
+      <h1>About</h1>
+      <p>Structural isomers calculator app</p>
+      <p>Version: v1.0.0</p>
+      <p>This app only accepts organic molecules.</p>
+      <p>
+        We are aware that some of the results of the calculator are impossible
+        isomers (for instance with azotes). This is caused by the tool used to
+        generate them (M. M. Jaghoori et al., ‘PMG: Multi-core metabolite
+        identification’, Electron. Notes Theor. Comput. Sci., vol. 299, pp.
+        53–60, 2013.).
+      </p>
+      <p>
+        Github repository:{' '}
+        <a
+          className="link"
+          href="https://github.com/opatiny/react-mol-structures"
+        >
+          https://github.com/opatiny/react-mol-structures
+        </a>
+      </p>
+    </div>
   );
 }
-
-const aboutStyle = {
-  padding: '10px',
-};
